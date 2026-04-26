a20f3861e9a14f5e45dfde6bf166abcc2c6ff632
sunnyvia <oktavia.anggarani.irawan@gmail.com>
2026-04-26 08:20:56 +0700

    Refactor home.dart to include HomeScreen widget

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Home")),
+      body: const Center(child: Text("HOME")),
+    );
+  }
+}
 void main() {
   runApp(const MyApp());
 }
